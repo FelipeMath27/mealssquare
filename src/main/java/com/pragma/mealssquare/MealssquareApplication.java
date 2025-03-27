f5bee5861e4fc9b697f180650d548d7c7ccb8c42
FelipeMath27 <felipesm27@gmail.com>
2025-03-26 19:15:52 -0500

    creacion proyecto spring para plazoleta de comidas

---
+++ src/main/java/com/pragma/mealssquare/MealssquareApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.mealssquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealssquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealssquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/mealssquare/MealssquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.mealssquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealssquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
